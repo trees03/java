b2ba5e419c3c5169ff08266a65a9b3999a832a7a
trees03 <80644823+trees03@users.noreply.github.com>
2023-06-11 00:25:07 +0530

    Add files via upload

---
+++ VectorDemo.java
@@ -0,0 +1,18 @@
+/*vector
+ * featured method
+ * dynamic array
+ * grow and shrink
+ * it is poor performer in case of insertion,
+ * deletion and searching
+ * not support concurrent accession(task in||)
+ * i.e we cannot perform over multiple 
+ * element at the same time
+*/
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
