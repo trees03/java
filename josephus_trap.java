1b987154701c1024ea61afb8c581e1cfe993ae96
trees03 <80644823+trees03@users.noreply.github.com>
2023-06-11 00:21:27 +0530

    Add files via upload

---
+++ josephus_trap.java
@@ -0,0 +1,16 @@
+package dsa;
+//n=77=64+13=2^6+l; W(n)=2l+1
+import java.util.*;
+public class josephus_trap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		for(int i=1;i<)
+		
+
+	}
+
+}
