1810f9e8ee68a0113144dffecb7b178393dfd90e
trees03 <80644823+trees03@users.noreply.github.com>
2023-06-11 00:11:06 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -5,7 +5,7 @@ public class addition {
 	public static void main(String[] args) {
 		int a = 10 ;
 		int b = 20 ;
-		int c = a + b;
+		int c = a + b ;
 		System.out.println(c);
 		// TODO Auto-generated method stub
 
