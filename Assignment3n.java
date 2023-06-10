b826dd70bfafe3079b13984769c434a1414566b5
trees03 <80644823+trees03@users.noreply.github.com>
2023-06-11 00:32:21 +0530

    Add files via upload

---
+++ Assignment3n.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Mom
+{
+	ArrayList<String> dish=new ArrayList<String>();
+}
+class Dish
+{
+	public int op;
+	public String name;
+	public String[] d={"Biriyani","Masala Dosa","noodles","pancake"};
+	int n=d.length;
+	public void print(){
+	
+	for(int i=0;i<n;i++)
+	{
+		System.out.println(i+1+d[i]);
+	}
+}
+}
+public class Assignment3n {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> list=Arrays.asList(d);
+		Dish d=new Dish();
+		list.Ar
+		d.print();
+		System.out.println("Enter your choice");
+		Scanner sc=new Scanner(System.in);
+		int c=sc.nextInt();
+		switch(c) {
+		case(1):
+			
+		}
+	}
+
+}
