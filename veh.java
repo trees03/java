f2a7b7617c67d0eb08cd1fa0bef8112652bf8689
trees03 <80644823+trees03@users.noreply.github.com>
2023-06-11 00:22:20 +0530

    Add files via upload

---
+++ veh.java
@@ -0,0 +1,21 @@
+package dsa;
+import java.io.*;
+public class veh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int v=200;
+//		int w=540;
+//		int f=w/2-v;
+//		int t=v-f;
+//		System.out.println("The number of two wheelers is :" +t);
+//		System.out.println("The number of four wheelers is :" +f);
+		// blokc swap
+
+
+		
+
+	}
+
+	
+}
