3925ceaf6c1f0dcb668a5c936b055f33817c6796
trees03 <80644823+trees03@users.noreply.github.com>
2023-06-11 00:20:49 +0530

    Add files via upload

---
+++ ring_route_bus.java
@@ -0,0 +1,25 @@
+package dsa;
+import java.util.*;
+public class ring_route_bus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the source:");
+		String s=sc.next();
+		System.out.println("Enter the destination:");
+		String d=sc.next();
+		Dictionary dict=new Hashtable();
+		dict.put('a', 800);
+		dict.put('b', 600);
+		dict.put('c', 750);
+		dict.put('d', 900);
+		dict.put('e', 1400);
+		dict.put('f', 1200);
+		dict.put('g', 1100);
+		dict.put('h', 1500);
+
+
+	}
+
+}
