07a0a8f605e8223c6c2b0c0ebe9924f0cd4e432d
trees03 <80644823+trees03@users.noreply.github.com>
2023-06-11 00:20:30 +0530

    Add files via upload

---
+++ digsum.java
@@ -0,0 +1,61 @@
+package dsa;
+import java.lang.Math;
+import java.util.Scanner;
+public class digsum {
+	public static void main(String[] args) {
+		        Scanner sc=new Scanner(System.in);
+//		        float n=sc.nextFloat();
+//		        int sum=0;
+//		        while(n!=0){
+//		            float d=n%10;
+//		            sum+=d;
+//		            n=n/10;
+//		        }
+//		        System.out.println(sum);
+		    
+	//given an int arr of size n. the task is to find the count of elements
+	//whose value is greater than all of its prior elements. the first element 
+	//of the array should
+	// also be counted to the count of the result
+	//eg: [7,4,8,2,9], [3,4,5,8,9], [3,4,5,8,9,9]=5
+	//1<=n<=20, 1<=arr[i]<=10,000
+		        
+		        
+//	System.out.println("Enter the size of the array");
+//	int n=sc.nextInt();
+//	int c=1;
+//	int[] arr=new int[n];
+//	for(int i=0;i<n;i++) {
+//		System.out.println("Enter the value");
+//		arr[i]=sc.nextInt();
+//	}
+//	for(int i=1;i<n;i++) {
+//		for(int j=i-1;j>=0;j--) {
+//			if(arr[i]<=arr[j]) {
+//				break;
+//			}
+//			else {
+//				if(j==0) {
+//					c++;
+//				}
+//			}
+//		}
+//	}
+//System.out.println(c);
+
+// a parking lot in a mall has rxc no. of parking spaces. each parking space will 
+//either be empty 0 or full 1 where status 0/1 of parking space is represented as 
+//the element of the matrix.
+// the task is to find the index of per
+	
+	
+//sweet 17: given a max no of 4 digits to the base 17 where a series of 
+//10a,11b,12c,13d,., 16-g as input
+//op its decimal value. ip is 1a expected op=27
+	//String s=sc.next();
+	
+	
+	
+	}
+}
+
