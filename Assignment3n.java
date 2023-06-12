4a7746ed636fad7194a520e3ce1e7aae8e7ea3a4
trees03 <80644823+trees03@users.noreply.github.com>
2023-06-12 14:19:05 +0530

    Update Assignment3n.java

--- Assignment3n.java
+++ Assignment3n.java
@@ -7,7 +7,7 @@ class Dish
 {
 	public int op;
 	public String name;
-	public String[] d={"Biriyani","Masala Dosa","noodles","pancake"};
+	public String[] d={"Biriyani","Masala Dosa","oodles","pancake"};
 	int n=d.length;
 	public void print(){
 	
